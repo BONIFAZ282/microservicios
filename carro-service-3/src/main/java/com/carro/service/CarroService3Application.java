14b4314caa1e6c7846cf24d10792214a238c301c
BONIFAZ282 <bonihd48@gmail.com>
2023-07-04 12:36:02 -0500

    rest template - feign client

---
+++ carro-service-3/src/main/java/com/carro/service/CarroService3Application.java
@@ -0,0 +1,13 @@
+package com.carro.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarroService3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarroService3Application.class, args);
+	}
+
+}
---
+++ usuario-service-3/src/test/java/com/usuario/service/UsuarioService3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usuario.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioService3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
